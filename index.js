d5655fddf77b2df5d205849b5475f0d1c615f091
卫慧杰 <w920098695@sina.cn>
2022-06-14 00:12:08 +0800

    ci 创建 create project 项目命令

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 const { program } = require('commander')
 const helpOptions = require('./lib/core/help')
-
+const createCommans = require('./lib/core/create')
 
 // 查看版本号
 program.version(require('./package.json').version)
 
 // 帮助和可选信息
 helpOptions()
+
+// 创建其指令
+createCommans()
 
 program.parse(program.argv)
