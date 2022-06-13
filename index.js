14b526b397ad311c1e6cde4e463b2af0527ff159
卫慧杰 <w920098695@sina.cn>
2022-06-13 23:41:52 +0800

    ci version-help-option 开发

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
-const program = require('commander')
+const { program } = require('commander')
+const helpOptions = require('./lib/core/help')
 
+
+// 查看版本号
 program.version(require('./package.json').version)
+
+// 帮助和可选信息
+helpOptions()
 
 program.parse(program.argv)
